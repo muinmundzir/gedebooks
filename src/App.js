977171168d4f7c2253dc80c5bd2af91fe6742525
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2022-01-06 17:03:15 +0700

    edit: import components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Book from './components/Book'
 import Navbar from './components/Navbar'
 
 function App() {
-  return <Navbar />
+  return (
+    <>
+      <Navbar />
+      <Book />
+    </>
+  )
 }
 
 export default App
