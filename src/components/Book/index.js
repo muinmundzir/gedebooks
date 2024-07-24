bbae0f31440d611b09532a8eb089dd7ea8927029
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-07-24 11:48:46 +0700

    feat: update endpoint

--- src/components/Book/index.js
+++ src/components/Book/index.js
@@ -1,66 +1,71 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-import './Book.css'
-import BookForm from './BookForm'
-import BookList from './BookList'
+import "./Book.css";
+import BookForm from "./BookForm";
+import BookList from "./BookList";
 
 const Book = () => {
-  const [booksData, setBooksData] = useState([])
+  const [booksData, setBooksData] = useState([]);
   const [formData, setFormData] = useState({
-    author: '',
-    id: '',
-    month_of_published: '',
-    synopsis: '',
-    title: '',
-    listed: '',
-    year_of_published: '',
-  })
+    author: "",
+    id: "",
+    month_of_published: "",
+    synopsis: "",
+    title: "",
+    listed: "",
+    year_of_published: "",
+  });
 
   const getBooks = async () => {
-    const result = await axios.get('http://localhost:3000/books')
-    setBooksData(result.data)
-  }
+    const result = await axios.get("https://react-gedebooks.netlify.app/books");
+    setBooksData(result.data);
+  };
 
   const handleAddBook = async (bookData) => {
     try {
-      await axios.post('http://localhost:3000/books', bookData)
-      getBooks()
+      await axios.post("https://react-gedebooks.netlify.app/books", bookData);
+      getBooks();
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   const handleDeleteBook = async (bookId) => {
     try {
-      await axios.delete(`http://localhost:3000/books/${bookId}`)
-      getBooks()
+      await axios.delete(`https://react-gedebooks.netlify.app/books/${bookId}`);
+      getBooks();
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   const handleEditBook = async (bookId) => {
     try {
-      const result = await axios.get(`http://localhost:3000/books/${bookId}`)
-      setFormData(result.data)
+      const result = await axios.get(
+        `https://react-gedebooks.netlify.app/books/${bookId}`,
+      );
+      setFormData(result.data);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   const handleUpdateBook = async (book) => {
     try {
-      await axios.patch(`http://localhost:3000/books/${book.id}`, book)
-      getBooks()
+      await axios.patch(
+        `https://react-gedebooks.netlify.app/books/${book.id}`,
+        book,
+      );
+      getBooks();
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
-    getBooks()
-  }, [])
+    getBooks();
+  }, []);
 
   return (
     <main className="main">
@@ -75,7 +80,7 @@ const Book = () => {
         editBookData={handleEditBook}
       />
     </main>
-  )
-}
+  );
+};
 
-export default Book
+export default Book;
